ee27577a401ad3ba082f944979602c30f1c55c5c
mnnandev <mnnanhavibhutta@gmail.com>
2023-07-20 03:47:02 -0600

    Updated code with fixed regular expression

--- src/components/videoPopup/VideoPopUp.jsx
+++ src/components/videoPopup/VideoPopUp.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactPlayer from "react-player/youtube";
 
-import "./VideoPopup.scss";
+import "./VideoPopUp.scss";
 
 const VideoPopup = ({ show, setShow, videoId, setVideoId }) => {
     const hidePopup = () => {
