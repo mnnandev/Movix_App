600d6f068e70251e192c2b5ca9f88ef55691c3f0
mnnandev <mnnanhavibhutta@gmail.com>
2023-07-20 02:57:57 -0600

    Updated code with fixed regular expression

--- src/pages/details/detailBanner/DetailBanner.jsx
+++ src/pages/details/detailBanner/DetailBanner.jsx
@@ -6,7 +6,7 @@ import dayjs from "dayjs";
 import "./DetailBanner.scss";
 
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
-import useFetch from "../../../hooks/useFetch";
+import useFetch from "../../../Hooks/useFetch";
 import Genres from "../../../components/genres/Genres";
 import CircleRating from "../../../components/circleRating/CircleRating";
 import Img from "../../../components/LazyloadImages/Images";
