dd684eb14c15aad9fd60ae816c924ea0399a9c2f
mnnandev <mnnanhavibhutta@gmail.com>
2023-07-20 03:34:43 -0600

    Updated code with fixed regular expression

--- src/pages/details/detailBanner/DetailBanner.jsx
+++ src/pages/details/detailBanner/DetailBanner.jsx
@@ -8,7 +8,7 @@ import "./DetailBanner.scss";
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 import useFetch from "../../../Hooks/useFetch";
 import Genres from "../../../components/Genres/Genres";
-import CircleRating from "../../../components/circleRating/CircleRating";
+import CircleRating from "../../../components/CircleRating/CircleRating";
 import Img from "../../../components/LazyloadImages/Images";
 import PosterFallback from "../../../assets/no-poster.png";
 import { PlayIcon } from "../Playbtn";
